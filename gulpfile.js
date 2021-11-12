d182ec5918bededfdf37b7e5e700c2a03ed5087a
HasibullahMahmood <HasibullahMahmood@gmail.com>
2021-11-12 20:22:46 +0300

    learnt how to add our sass and customize shinobi library without changing its code

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ const sass = require('gulp-sass')(require('sass'));
 const purgeCss = require('gulp-purgecss');
 
 function buildStyles() {
-	return src('shinobi/**/*.scss')
+	return src('sass/**/*.scss')
 		.pipe(sass())
 		.pipe(purgeCss({ content: ['*.html'] }))
 		.pipe(dest('css'));
 }
 
 function watchTask() {
-	watch(['shinobi/**/*.scss', '*.html'], buildStyles);
+	watch(['sass/**/*.scss', '*.html'], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
